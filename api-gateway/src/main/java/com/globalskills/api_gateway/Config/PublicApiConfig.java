3e1d1b02f076171534ed73386cf38e3336f2577e
QuanDong <quanonepunch@gmail.com>
2025-10-13 21:36:09 +0700

    add payment swagger public api

--- api-gateway/src/main/java/com/globalskills/api_gateway/Config/PublicApiConfig.java
+++ api-gateway/src/main/java/com/globalskills/api_gateway/Config/PublicApiConfig.java
@@ -13,6 +13,7 @@ public class PublicApiConfig {
             "/swagger-resources/**",
             "/api/user/v3/api-docs",
             "/api/forum/v3/api-docs",
+            "/api/payment/v3/api-docs",
             "/api/authentication/login",
             "/api/authentication/register",
             "/api/authentication/forgot-password"
